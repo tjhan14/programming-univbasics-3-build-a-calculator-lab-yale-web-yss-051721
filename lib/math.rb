7bd2a9b3733d47d009594931617f8a8b6bf22954
dependable-coder-f3d7 <tj.han@yale.edu>
2021-05-31 08:55:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,10 @@
 def addition(num1, num2)
-  num1.to_i + 
+  sum = (num1.to_i + num2.to_i)
+  puts sum
 end
 
 def subtraction(num1, num2)
-  return num2 - num1
+  sub = 
 end
 
 def division(num1, num2)
