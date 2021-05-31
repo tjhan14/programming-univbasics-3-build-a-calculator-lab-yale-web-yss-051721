d07248f09c3941ae8c47356f0d918d51820528f5
dependable-coder-f3d7 <tj.han@yale.edu>
2021-05-31 09:05:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,33 @@
 def addition(num1, num2)
-  sum = (num1.to_i + num2.to_i)
+  sum = (num1.to_f + num2.to_f)
   puts sum
+  return sum
 end
 
 def subtraction(num1, num2)
-  sub = (num1.to_i - num2.to_i)
+  sub = (num1.to_f - num2.to_f)
+  puts sub
+  return sub
 end
 
 def division(num1, num2)
-  return num2 / num1
+  div = num2.to_f / num1.to_f
+  puts div 
+  return div
 end
 
 def multiplication(num1, num2)
-
+  mult = (num1.to_f * num2.to_f)
+  puts mult
+  return mult 
 end
 
 def modulo(num1, num2)
-
+  modu = num2.to_f % num1.to_f
+  puts modu 
+  return modu
 end
 
 def square_root(num)
-
+  Math.sqrt(num.to_f)
 end
